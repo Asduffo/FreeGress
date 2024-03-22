6603f19f090c13d09427823db8deb1f6632278f2
Asduffo <Asduffo@users.noreply.github.com>
2024-03-22 09:53:08 +0100

    Forgot one small thing

--- src/execute_cb_test.sh
+++ src/execute_cb_test.sh
@@ -5,6 +5,5 @@ for i in 200 400 600 800
 do
     python3 guidance/main_guidance.py +experiment=zinc250k.yaml dataset=zinc250k general.wandb="online" general.name="cb_zinc_"$NAMES"_nodeinfer_test_"$i \
     guidance.guidance_target=\[$1\] +general.test_samples_to_generate=100 +general.test_only=$2 general.gpus=[3] guidance.guidance_medium="NONE" \
-    train.batch_size=1  +general.trained_regressor_path=$3 +guidance.lambda_guidance=$i \
-    guidance.node_model_path="/disk1/m.ninniri/FreeGress/outputs/2024-02-20/11-40-03-nodemodel_mw/checkpoints/nodemodel_mw/last.ckpt"; 
+    train.batch_size=1  +general.trained_regressor_path=$3 +guidance.lambda_guidance=$i
 done
